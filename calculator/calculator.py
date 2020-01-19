1dd1bc883db4283555c3442924019fb2be1ab1b1
John <starky.satyam@gmail.com>
2020-01-19 12:25:56 +0530

    adding calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,9 @@
+"""
+calculator library containing basic function 
+"""
+
+def add(first_term,second_term):
+    return first_term + second_term
+    
+def subtract(first_term,second_term):
+    return first_term - second_term
